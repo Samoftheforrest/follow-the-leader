df803e2a7b2afe02cf30afa2932bf36225c4dd7f
Sam Forrest <samforrest@live.co.uk>
2022-03-10 19:45:11 +0000

    [AMEND] formatted HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,11 @@ let squares = document.querySelector('.squares');
 let difficultyBtns = document.querySelectorAll('.intro-btn');
 let [easyBtn, mediumBtn, hardBtn] = difficultyBtns;
 
+let playerPosition = 0;
+let leaderPosition = 0;
+let leader = '<div class="leader"></div>';
+let player = '<div class="player"></div>'
+
 // difficulty settings
 const difficulties = [{
     mode: 'Easy',
@@ -27,12 +32,29 @@ const startGame = function(mode) {
         area.classList.add('d-none');
     });
     gameBoard.classList.remove('d-none');
+    generateSquares(mode);
+    leadersTurn(mode);
+}
+
+/**This function generates the amount of sqaures based off the mode's squares in the difficulties object and sets their width */
+const generateSquares = function(mode) {
     for (let i = 0; i < difficulties[mode].squares; i++) {
         let square = document.createElement('div');
         square.classList.add('square');
+        square.classList.add(i);
         square.style.width = `${100 / (Math.sqrt(difficulties[mode].squares))}%`;
         square.style.height = `${100 / (Math.sqrt(difficulties[mode].squares))}%`;
         squares.append(square);
+    }
+};
+
+const leadersTurn = function(mode) {
+    let square = document.querySelectorAll('.square');
+    let randomNumber = Math.floor(Math.random() * 2);
+    square[leaderPosition].innerHTML = leader;
+
+    let leaderMoves = function() {
+        
     }
 }
 
